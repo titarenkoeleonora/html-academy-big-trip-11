680b79fde69d706e76a38af22f37956d6432903e
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-04-10 23:46:36 +0300

    Генерирует объект точки путешествия

---
+++ src/components/constants.js
@@ -0,0 +1,2 @@
+export const CITIES = [`Amsterdam`, `New-York`, `Moscow`, `Paris`, `Berlin`, `Saint-Petersburg`];
+export const TRIP_POINT_TYPES = [`Taxi`, `Bus`, `Train`, `Ship`, `Transport`, `Drive`, `Flight`, `Check`, `Sightseeing`, `Restaurant`];
--- src/components/mock/route-point.js
+++ src/components/mock/route-point.js
@@ -1,57 +1,4 @@
-import {createCostTemplate} from "./components/cost.js";
-import {createDayTemplate} from "./components/day.js";
-import {createEventEditTemplate} from "./components/event-edit.js";
-import {createEventsListTemplate} from "./components/events-list.js";
-import {createFiltersTemplate} from "./components/filters.js";
-import {createRoutePointsTemplate} from "./components/route-points.js";
-import {createRouteTemplate} from "./components/route.js";
-import {createSiteMenuTemplate} from "./components/site-menu.js";
-import {createSortingTemplate} from "./components/sotring.js";
-import {createDaysContainerTemplate} from "./components/trip-days.js";
-import {createTripInfoTemplate} from "./components/trip-info.js";
-import {TRIP_POINT_TYPES, CITIES} from "./components/constants.js";
-
-const POINTS_COUNT = 3;
-
-const tripMainElement = document.querySelector(`.trip-main`);
-const tripControlsElement = tripMainElement.querySelector(`.trip-controls`);
-const tripControlsTitlesElement = tripControlsElement.querySelectorAll(`h2`);
-const [firstTitleElement, secondTitleElement] = tripControlsTitlesElement;
-const siteMainElement = document.querySelector(`.page-main`);
-const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
-
-const render = (container, template, place = `beforeend`) => container.insertAdjacentHTML(place, template);
-
-render(tripMainElement, createTripInfoTemplate(), `afterbegin`);
-
-const tripInfoElement = tripMainElement.querySelector(`.trip-info`);
-
-render(tripInfoElement, createRouteTemplate());
-render(tripInfoElement, createCostTemplate());
-
-render(firstTitleElement, createSiteMenuTemplate(), `afterend`);
-render(secondTitleElement, createFiltersTemplate(), `afterend`);
-render(tripEventsElement, createSortingTemplate());
-render(tripEventsElement, createEventEditTemplate());
-render(tripEventsElement, createDaysContainerTemplate());
-
-const daysContainerElement = tripEventsElement.querySelector(`.trip-days`);
-
-render(daysContainerElement, createDayTemplate());
-
-const dayElement = daysContainerElement.querySelector(`.day`);
-
-render(dayElement, createEventsListTemplate());
-
-const eventsListElement = dayElement.querySelector(`.trip-events__list`);
-
-const renderRoutePoints = (container, template, place) => {
-  for (let i = 0; i < POINTS_COUNT; i++) {
-    render(container, template, place);
-  }
-};
-
-renderRoutePoints(eventsListElement, createRoutePointsTemplate());
+import {CITIES, TRIP_POINT_TYPES} from "../constants";
 
 const TripDescriptions = [
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
@@ -131,7 +78,7 @@ const formatTime = () => {
 const getRandomDate = () => {
   const targetDate = new Date();
   const sign = Math.random() > 0.5 ? 1 : -1;
-  const diffValue = sign * getRandomInteger(0, 10);
+  const diffValue = sign * getRandomInteger(1, 10);
 
   const dd = targetDate.getDate() + diffValue;
   const mm = targetDate.getMonth() + 1;
@@ -169,6 +116,3 @@ const generateTripPoint = () => {
     offers: [],
   };
 };
-
-console.log(generateTripPoint());
-
