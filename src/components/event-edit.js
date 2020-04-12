22926e91853469d9e5206b0f4e3ca9bb5a1a7658
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-04-12 11:38:08 +0300

    \

--- src/components/event-edit.js
+++ src/components/event-edit.js
@@ -54,7 +54,7 @@ export const createEventEditTemplate = (tripPoint) => {
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
