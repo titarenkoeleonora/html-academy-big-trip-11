c910affdae0d881c38bf6bd8926bbcca338e9947
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-04-12 11:55:56 +0300

    Исправляешь ошибки в значках транспорта

--- src/components/route-points.js
+++ src/components/route-points.js
@@ -19,7 +19,7 @@ export const createRoutePointsTemplate = (tripPoint) => {
     `<li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
         </div>
         <h3 class="event__title">${type} ${typeRoutePointMap[type]} ${destination.name}</h3>
 
