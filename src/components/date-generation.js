64d57d7df60179fb4d0bd04a4f1aaac5fe6c1bba
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-04-15 01:18:42 +0300

    Исправляет счетчик разницы времени

--- src/components/date-generation.js
+++ src/components/date-generation.js
@@ -1,15 +1,17 @@
 import {getRandomInteger, getRandomDate} from "../utils";
 import {HOURS_IN_MS_COEFFICIENT, MINUTES_IN_DAY, HOURS_IN_DAY, MINUTES_IN_HOUR} from "./constants";
 
-export const getRandomMillisecondsCount = () => (getRandomInteger(10, MINUTES_IN_HOUR) * getRandomInteger(1, 5)) * getRandomInteger(1, 10);
-
-const сonvertToMinutes = (timeInMilliseconds) => Math.floor(timeInMilliseconds * HOURS_IN_MS_COEFFICIENT) / MINUTES_IN_HOUR;
+const MAX_COUNT_DAYS_DIFFERENCE = 1;
 
 export const startDate = () => getRandomDate();
 
 export const endDate = (date) => {
-  const randomMillisecondsCount = getRandomMillisecondsCount();
-  return new Date(Date.parse(date) + Math.floor(сonvertToMinutes(randomMillisecondsCount)));
+  const targetDate = new Date(date);
+
+  targetDate.setDate(targetDate.getDate() + getRandomInteger(0, MAX_COUNT_DAYS_DIFFERENCE));
+  targetDate.setHours(targetDate.getHours() + getRandomInteger(0, HOURS_IN_DAY));
+  targetDate.setMinutes(targetDate.getMinutes() + getRandomInteger(0, MINUTES_IN_HOUR));
+  return targetDate;
 };
 
 export const getTimeDifference = (start, end) => {
@@ -19,8 +21,13 @@ export const getTimeDifference = (start, end) => {
   let difference = null;
 
   if (randomDifferenceMinutes >= MINUTES_IN_DAY) {
-    difference = `${days + `D`} ${hours + `H`} ${Math.floor(randomDifferenceMinutes % MINUTES_IN_HOUR) + `M`}`;
-    return difference;
+    if (hours > 0) {
+      difference = `${days + `D`} ${hours + `H`} ${Math.floor(randomDifferenceMinutes % MINUTES_IN_HOUR) + `M`}`;
+      return difference;
+    } else {
+      difference = `${days + `D`} ${Math.floor(randomDifferenceMinutes % MINUTES_IN_HOUR) + `M`}`;
+      return difference;
+    }
   } else if (randomDifferenceMinutes <= MINUTES_IN_HOUR) {
     difference = Math.floor(randomDifferenceMinutes) + `M`;
     return difference;
